9cfdbccb7687c711addd8c60129edb6b3f379081
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-31 17:14:06 +0300

    create Pin and Comment typeDefs

--- typeDefs.js
+++ typeDefs.js
@@ -1,17 +1,32 @@
-const {gql} = require("apollo-server")
+const { gql } = require("apollo-server");
 
 module.exports = gql`
+	type User {
+		_id: ID
+		name: String
+		email: String
+		picture: String
+	}
 
-type User {
-  _id: ID
-  name: String
-  email: String
-  picture: String
-}
+	type Pin {
+		_id: ID
+		createdAt: String
+		title: String
+		content: String
+		image: String
+		latitude: Float
+		longitude: Float
+		author: User
+		comments: [Comment]
+	}
 
-type Query {
-  me:User
-  
-}
+	type Comment {
+		text: String
+		createdAt: String
+		Author: User
+	}
 
-`
+	type Query {
+		me: User
+	}
+`;
