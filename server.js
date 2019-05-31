febe27f190d7f4ac71a2c4708b26d214e8e5dc3a
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-31 17:09:15 +0300

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+require("dotenv").config();
+
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(_ => {
+		console.log("DB connected");
+	})
+	.catch(err => console.error(err));
 
 const server = new ApolloServer({
 	typeDefs,
