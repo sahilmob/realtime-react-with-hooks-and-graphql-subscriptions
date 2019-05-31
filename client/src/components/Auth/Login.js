9c3d27d888b05ed80da54abf64754ac3e2b9f2cb
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-31 17:54:46 +0300

    implement google oAuth login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,32 @@
+import { GoogleLogin } from "react-google-login";
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+	const onSuccess = googleUser => {
+		const idToken = googleUser.getAuthResponse().id_token;
+		console.log(idToken);
+	};
+
+	return (
+		<GoogleLogin
+			clientId="975756276358-ef53nqbi7p4pioe1r54vmqi5vplvj76m.apps.googleusercontent.com"
+			onSuccess={onSuccess}
+			isSignedIn={true}
+		/>
+	);
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+	root: {
+		height: "100vh",
+		display: "flex",
+		justifyContent: "center",
+		flexDirection: "column",
+		alignItems: "center"
+	}
 };
 
 export default withStyles(styles)(Login);
