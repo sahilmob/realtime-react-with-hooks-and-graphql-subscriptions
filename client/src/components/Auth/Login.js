08c7cabcc2dcbb442a1cd28d1002fa57c27b8c15
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-31 19:20:22 +0300

    execute ME query

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,13 +1,29 @@
 import { GoogleLogin } from "react-google-login";
+import { GraphQLClient } from "graphql-request";
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+query{
+  me{
+    _id
+    name
+    email
+    picture
+  }
+}
+`;
+
 const Login = ({ classes }) => {
-	const onSuccess = googleUser => {
+	const onSuccess = async googleUser => {
 		const idToken = googleUser.getAuthResponse().id_token;
-		console.log(idToken);
+		const client = new GraphQLClient("http://localhost:4000/graphql", {
+			headers: { authorization: idToken }
+		});
+		const data = await client.request(ME_QUERY);
+		console.log(data);
 	};
 
 	return (
