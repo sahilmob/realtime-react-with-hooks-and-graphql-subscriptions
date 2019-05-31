4add51cf76e278b294b11a0bfd1dc151d2e554ba
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-31 19:19:29 +0300

    implement authenticated HOF

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,21 @@
+const { AuthenticationError } = require("apollo-server");
+
 const user = {
 	_id: "1",
 	name: "Sahil",
 	email: "Sahil@test.com",
 	picture: "http://cloudinary.com/erdhgre"
 };
+
+const authenticated = next => (root, args, ctx, info) => {
+	if (!ctx.currentUser) {
+		throw new AuthenticationError("You must be logged in");
+	}
+	return next(root, args, ctx, info);
+};
+
 module.exports = {
 	Query: {
-		me: () => user
+		me: authenticated((root, args, ctx, info) => ctx.currentUser)
 	}
 };
