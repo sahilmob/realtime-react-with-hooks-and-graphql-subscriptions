1be73d74e06ad780ed00fe0228da6caff7f5d470
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-01 17:51:04 +0300

    build no content component

--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,21 +1,35 @@
+import ExploreIcon from "@material-ui/icons/Explore";
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-// import Explore from "@material-ui/icons/Explore";
-// import Typography from "@material-ui/core/Typography";
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+const NoContent = ({ classes }) => (
+	<div className={classes.root}>
+		<ExploreIcon className={classes.icon} />
+		<Typography
+			noWrap
+			component="h2"
+			variant="h6"
+			align="center"
+			color="textPrimary"
+			gutterBottom
+		>
+			Click on the map to add a pin
+		</Typography>
+	</div>
+);
 
 const styles = theme => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  icon: {
-    margin: theme.spacing.unit,
-    fontSize: "80px"
-  }
+	root: {
+		display: "flex",
+		alignItems: "center",
+		flexDirection: "column",
+		justifyContent: "center"
+	},
+	icon: {
+		margin: theme.spacing.unit,
+		fontSize: "80px"
+	}
 });
 
 export default withStyles(styles)(NoContent);
