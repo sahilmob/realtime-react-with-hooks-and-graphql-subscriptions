03066474d7dbebbfec2c46235cfbf6e7f5a74b88
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-01 17:30:36 +0300

    render BlogContent conditionally

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import Context from "../context";
+import CreatePin from "./Pin/CreatePin";
+import NoContent from "./Pin/NoContent";
+import { Paper } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-// import { Paper } from "@material-ui/core";
 
 const Blog = ({ classes }) => {
-  return <div>Blog</div>;
+	const { state } = useContext(Context);
+	const { draft } = state;
+
+	let BlogContent;
+
+	if (!draft) {
+		BlogContent = NoContent;
+	} else if (draft) {
+		BlogContent = CreatePin;
+	}
+	return (
+		<Paper className={classes.root}>
+			<BlogContent />
+		</Paper>
+	);
 };
 
 const styles = {
-  root: {
-    minWidth: 350,
-    maxWidth: 400,
-    maxHeight: "calc(100vh - 64px)",
-    overflowY: "scroll",
-    display: "flex",
-    justifyContent: "center"
-  },
-  rootMobile: {
-    maxWidth: "100%",
-    maxHeight: 300,
-    overflowX: "hidden",
-    overflowY: "scroll"
-  }
+	root: {
+		minWidth: 350,
+		maxWidth: 400,
+		maxHeight: "calc(100vh - 64px)",
+		overflowY: "scroll",
+		display: "flex",
+		justifyContent: "center"
+	},
+	rootMobile: {
+		maxWidth: "100%",
+		maxHeight: 300,
+		overflowX: "hidden",
+		overflowY: "scroll"
+	}
 };
 
 export default withStyles(styles)(Blog);
