bfeaccc0f9d3caeb05bfbe1b5244e2f13dc7bc41
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-01 16:31:53 +0300

    render map

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
 import React from "react";
 import withRoot from "../withRoot";
+import Map from "../components/Map"
 
 const App = () => {
 	return (
-		<div>
+		<>
 			<Header />
-		</div>
+			<Map />
+		</>
 	);
 };
 
