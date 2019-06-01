dc7ed0a4a78bf11181dc3b3bc8ae486b786824c2
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-01 12:50:02 +0300

    build header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Context from "../context";
+import MapIcon from "@material-ui/icons/Map";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
+	const { state } = useContext(Context);
+	const { currentUser } = state;
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<div className={classes.grow}>
+						<MapIcon className={classes.icon} />
+						<Typography component="h1" variant="h6" color="inherit" noWrap>
+							GeoPins
+						</Typography>
+					</div>
+					{currentUser && (
+						<div className={classes.grow}>
+							<img
+								className={classes.picture}
+								src={currentUser.picture}
+								alt={currentUser.name}
+							/>
+							<Typography variant="h5" color="inherit" noWrap>
+								{currentUser.name}
+							</Typography>
+						</div>
+					)}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 };
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
-  },
-  icon: {
-    marginRight: theme.spacing.unit,
-    color: "green",
-    fontSize: 45
-  },
-  mobile: {
-    display: "none"
-  },
-  picture: {
-    height: "50px",
-    borderRadius: "90%",
-    marginRight: theme.spacing.unit * 2
-  }
+	root: {
+		flexGrow: 1
+	},
+	grow: {
+		flexGrow: 1,
+		display: "flex",
+		alignItems: "center"
+	},
+	icon: {
+		marginRight: theme.spacing.unit,
+		color: "green",
+		fontSize: 45
+	},
+	mobile: {
+		display: "none"
+	},
+	picture: {
+		height: "50px",
+		borderRadius: "90%",
+		marginRight: theme.spacing.unit * 2
+	}
 });
 
 export default withStyles(styles)(Header);
