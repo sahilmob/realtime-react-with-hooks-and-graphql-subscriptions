9a1985c83cf9d483d1a43cf64ef94cb7d72f1e65
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-01 17:22:53 +0300

    add draft related action types

--- client/src/actionTypes.js
+++ client/src/actionTypes.js
@@ -1,3 +1,5 @@
 export const LOGIN_USER = "LOGIN_USER";
 export const IS_LOGGED_IN = "IS_LOGGED_IN";
 export const SIGNOUT_USER = "SIGNOUT_USER";
+export const CREATE_DRAFT = "CREATE_DRAFT";
+export const UPDATE_DRAFT_LOCATION = "UPDATE_DRAFT_LOCATION";
