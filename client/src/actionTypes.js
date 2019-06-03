4961e86af077c1bcd7569f1d48c556a5bde7460d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-03 19:24:34 +0300

    add CREATE_COMMENT action type

--- client/src/actionTypes.js
+++ client/src/actionTypes.js
@@ -8,3 +8,4 @@ export const GET_PINS = "GET_PINS";
 export const CREATE_PIN = "CREATE_PIN";
 export const SET_PIN = "SET_PIN";
 export const DELETE_PIN = "DELETE_PIN";
+export const CREATE_COMMENT = "CREATE_COMMENT";
