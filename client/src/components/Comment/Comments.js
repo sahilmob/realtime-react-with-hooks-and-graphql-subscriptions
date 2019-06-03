70240c74fbec89d5dc659b3b9ee2caf4adba646b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-03 18:32:38 +0300

    build component

--- client/src/components/Comment/Comments.js
+++ client/src/components/Comment/Comments.js
@@ -1,22 +1,44 @@
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-// import Avatar from "@material-ui/core/Avatar";
-// import Typography from "@material-ui/core/Typography";
 
-const Comments = ({ classes }) => <div>Comments</div>;
+const Comments = ({ comments, classes }) => (
+	<List className={classes.root}>
+		{comments.map((comment, i) => (
+			<ListItem key={i} alignItems="flex-start">
+				<ListItemAvatar>
+					<Avatar src={comment.author.picture} alt={comment.author.name} />
+				</ListItemAvatar>
+				<ListItemText
+					primary={comment.text}
+					secondary={
+						<Typography
+							className={classes.inline}
+							component="span"
+							color="textPrimary"
+						>
+							{comment.author.name}
+						</Typography>
+					}
+				/>
+			</ListItem>
+		))}
+	</List>
+);
 
 const styles = theme => ({
-  root: {
-    width: "100%",
-    backgroundColor: theme.palette.background.paper
-  },
-  inline: {
-    display: "inline"
-  }
+	root: {
+		width: "100%",
+		backgroundColor: theme.palette.background.paper
+	},
+	inline: {
+		display: "inline"
+	}
 });
 
 export default withStyles(styles)(Comments);
