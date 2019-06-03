cdbb8c5d86315d101b10b667e70a8675646607e5
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-03 16:08:54 +0300

    add getPins resolver

--- resolvers.js
+++ resolvers.js
@@ -11,7 +11,13 @@ const authenticated = next => (root, args, ctx, info) => {
 
 module.exports = {
 	Query: {
-		me: authenticated((root, args, ctx, info) => ctx.currentUser)
+		me: authenticated((root, args, ctx, info) => ctx.currentUser),
+		getPins: async (root, args, ctx) => {
+			const pins = await Pin.find({})
+				.populate("author")
+				.populate("comments.author");
+			return pins;
+		}
 	},
 	Mutation: {
 		createPin: async (root, args, ctx, info) => {
