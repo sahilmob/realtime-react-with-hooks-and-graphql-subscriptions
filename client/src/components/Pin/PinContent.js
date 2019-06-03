6ad7d86e19b17fd16523c00ef779e6a9a4cba66d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-03 18:06:51 +0300

    build pin content component

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import Context from "../../context";
+import FaceIcon from "@material-ui/icons/Face";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
-// import AccessTime from "@material-ui/icons/AccessTime";
-// import Face from "@material-ui/icons/Face";
 
 const PinContent = ({ classes }) => {
-  return <div>PinContent</div>;
+	const { state } = useContext(Context);
+	const { title, content, author, createdAt, comments } = state.currentPin;
+	return (
+		<div className={classes.root}>
+			<Typography component="h2" variant="h4" color="primary" gutterBottom>
+				{title}
+			</Typography>
+			<Typography
+				className={classes.text}
+				component="h3"
+				variant="h6"
+				color="inherit"
+				gutterBottom
+			>
+				<FaceIcon className={classes.icon} /> {author.name}
+			</Typography>
+			<Typography
+				className={classes.text}
+				variant="subtitle2"
+				color="inherit"
+				gutterBottom
+			>
+				<AccessTimeIcon />
+				{new Date(createdAt).toLocaleString()}
+			</Typography>
+			<Typography variant="subtitle1" gutterBottom>
+				{content}
+			</Typography>
+		</div>
+	);
 };
 
 const styles = theme => ({
-  root: {
-    padding: "1em 0.5em",
-    textAlign: "center",
-    width: "100%"
-  },
-  icon: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
-  },
-  text: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	root: {
+		padding: "1em 0.5em",
+		textAlign: "center",
+		width: "100%"
+	},
+	icon: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit
+	},
+	text: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
 
 export default withStyles(styles)(PinContent);
